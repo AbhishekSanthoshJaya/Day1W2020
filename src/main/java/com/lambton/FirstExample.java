c8276c76c9f7978b4ba47be156096298b5ba688e
MacStudent <macstudent@128-iMAC03.local>
2020-01-21 20:48:52 -0500

    [Abhishek] Initial commit

---
+++ src/main/java/com/lambton/FirstExample.java
@@ -0,0 +1,12 @@
+package com.lambton;
+
+public class FirstExample {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to JAVA programming");
+        for (int i=0;i<10;i++) {
+            System.out.println("Abhishek Santhosh Jaya");
+        }
+
+    }
+}
