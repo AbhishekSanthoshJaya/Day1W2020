3b2b2aadd65bcfbdf97a892b39c0dee945615461
AbhishekSanthoshJaya <abhisheksj35@gmail.com>
2020-01-22 21:23:34 -0500

    Day 2 - Data types, variable declaration and type conversion

---
+++ src/main/java/com/lambton/StringsExamples.java
@@ -0,0 +1,72 @@
+package com.lambton;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class StringsExamples
+{
+
+    public static void main(String[] args) {
+/*
+    String s1 = "Hello";
+    String s2 = "World";
+    String s3 = "Hello";
+    String s4 = s1;
+
+    String s5 = new String(s2);
+    String s6 = new String();
+    s6 = "World";
+        String s7 = new String ( "Hello");
+        System.out.println("S1 = " + s1);
+        System.out.println("S2 = " + s2);
+        System.out.println("S3 = " + s3);
+        System.out.println("S4 = " + s4);
+        System.out.println("S5 = " + s5);
+        System.out.println("S6 = " + s6);
+
+        if(s2 == s6)
+        {
+            System.out.println("s2(" + s2 + ") = s6(" + s6 + ")");
+        } else {
+            System.out.println("s2 (" + s2 + ") <> s6 (" + s6 + ")");
+        }
+
+        if (s2.equals(s6))
+        {
+            System.out.println("s2(" + s2+ ") equals s6 (" + s6 + ")");
+        } else {
+            System.out.println("s2(" + s2 + ") <> s6 (" + s6 +")");
+        }
+//Trying string handling functions
+
+        int a = 100;
+        int b = 200;
+        int c = a + b;
+        String str = String.format("%d + %d = %d", a, b, c);
+        System.out.println(str);
+
+        String betterString = String.join( "", "Welcome",  " to", " Lambton", " Toronto");
+        System.out.println(betterString);
+
+        char p = s1.;
+        System.out.println(p);
+
+*/
+
+    }
+
+    public void outputString (String result)
+    {
+        Scanner input = new Scanner(System.in);
+        String value;
+        System.out.println("Enter your string");
+        while (input.hasNext()){
+            value = input.next();
+            
+
+        }
+
+
+
+    }
+}
